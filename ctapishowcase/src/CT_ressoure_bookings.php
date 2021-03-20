e792ab8c156f4123317ffccc92644bc39a06de96
Bernhard Weichel <github.com@nospam.weichel21.de>
2021-03-20 10:53:35 +0100

    ressourcebookings now use helper method directly

--- ctapishowcase/src/CT_ressoure_bookings.php
+++
@@ -1,9 +0,0 @@
-<?php
-$url = "CT_getRessourceBookings($ajax_domain)";
-$report = [
-    'function' => "CT_getRessourceBookings($ajax_domain)",
-];
-$allRessources = CT_APITOOLS\CT_getRessourceBookings($ajax_domain);
-
-$report['response'] = $allRessources;
-
