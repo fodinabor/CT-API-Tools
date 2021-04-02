ed0680fd8462fbdab4275f63ad1e010f4cb37a77
Bernhard Weichel <github.com@nospam.weichel21.de>
2021-04-02 10:00:08 +0200

    now export a ruby file to be used as input for groupvisualizer. This can be used to attach authorization information to the gropu visualization

--- ctapishowcase/src/v1_churchauth--all.php
+++ ctapishowcase/src/v1_churchauth--all.php
@@ -12,6 +12,9 @@
  * WTF license
  */
 
+
+// helpers for JSONPath
+
 use Flow\JSONPath\JSONPath;
 
 /**
@@ -41,7 +44,12 @@ function find_one_in_JSONPath(&$masterdata, $jsonpath)
     return ($result);
 }
 
+// helpers for CT autho
+
 /**
+ *
+ * this resolves an authentry to a text description readable by humans
+ *
  * @param JSONPath $masterdata_jsonpath the loaded jason path
  * @param $auth_entry  the auth entra as it comes from CT
  * @return string[]  athe result of the evaluation
@@ -58,8 +66,11 @@ function resolve_auth_entry($auth_entry, JSONPath $masterdata_jsonpath, $datafie
         // todo fix handling of auth for subgroups
         // todo fix handling of auth parameters
 
+        // find in which table to resolv the authkey
+        // by default it is auth_table
         $lookuptable = isset($datafield) ? $datafield : "auth_table";
 
+        // auth_key = -1 resolves to all (groups etc.)
         if ($_auth_key == -1) {
             $__modulename = "alle";
         } else {
@@ -68,10 +79,13 @@ function resolve_auth_entry($auth_entry, JSONPath $masterdata_jsonpath, $datafie
                 $__authname = key_exists("auth", $__authrecord) ? " [{$__authrecord['auth']}]" : "";
                 $__modulename = "{$__authrecord['bezeichnung']}{$__authname}";
             } else {
-                $__modulename = "?? undefined in $lookuptable ??";
+                $__modulename = "$_auth_key undefined in $lookuptable ??";
             }
         }
 
+        // if resolved value is a nested auth record, we have to resolved this again.
+        // this means we have an auth entry with parameters.
+        // otherwise wer return the key as dummy parameter.
         $_authvalue_resolved = is_array($_authvalue) ?
             resolve_auth_entry($_authvalue, $masterdata_jsonpath, $__authrecord['datenfeld']) : $_auth_key;
 
@@ -99,9 +113,12 @@ function pushauthdef($hash, $role, $definition, &$authdefinitions)
 }
 
 
-// handle status
+// read auth record
 
 /**
+ *
+ * Read auth defined at the level of status (Member etc.)
+ *
  * @param JSONPath $masterdata_jsonpath
  * @param array $authdefinitions
  * @return array
@@ -119,7 +136,7 @@ function read_auth_by_status(JSONPath $masterdata_jsonpath, array &$authdefiniti
         $auth = isset($status['auth']) ? resolve_auth_entry($status['auth'], $masterdata_jsonpath) : [];
 
         $hash = hash('md5', json_encode($auth));
-        pushauthdef($hash, "Status: $statusname", $auth, $authdefinitions);
+        pushauthdef($hash, "ST $statusname", $auth, $authdefinitions);
 
         $result = [
             "auth_hash" => $hash,
@@ -133,6 +150,10 @@ function read_auth_by_status(JSONPath $masterdata_jsonpath, array &$authdefiniti
 
 
 /**
+ *
+ *  Read auth defined at the level of Grouptype
+ *  note this walks along grouatypes
+ *
  * @param JSONPath $masterdata_jsonpath
  * @param $authdefinitions
  * @return array
@@ -151,21 +172,23 @@ function read_auth_by_grouptypes(JSONPath $masterdata_jsonpath, &$authdefinition
         // get membertype
         $membertypes = find_in_JSONPath($masterdata_jsonpath,
             "$..grouptypeMemberstatus[?(@.gruppentyp_id == '$grouptypeid')]");
-        $r = array_map(function ($auth_entry) use ($grouptypename, $masterdata_jsonpath, &$authdefinitions) {
+        $r = array_map(function ($authentry) use ($grouptypename, $masterdata_jsonpath, &$authdefinitions) {
 
-            if (isset($auth_entry['auth'])) {
-                $auth = resolve_auth_entry($auth_entry['auth'], $masterdata_jsonpath);
+            if (isset($authentry['auth'])) {
+                $auth = resolve_auth_entry($authentry['auth'], $masterdata_jsonpath);
             } else {
                 $auth = [];
             }
 
             $hash = hash('md5', json_encode($auth));
 
-            pushauthdef($hash, "Gruppentyp: $grouptypename {$auth_entry['bezeichnung']}", $auth, $authdefinitions);
+            $gtabbreviation = explode(" ", $grouptypename)[0];
+            pushauthdef($hash, "GTRL $gtabbreviation {$authentry['bezeichnung']}", $auth, $authdefinitions);
 
             return [
+                'grouptypeMemberstatus_id' => $authentry['id'],
                 'grouptype' => $grouptypename,
-                'membertype' => $auth_entry['bezeichnung'],
+                'membertype' => $authentry['bezeichnung'],
                 "auth_hash" => $hash,
                 'auth' => $auth
             ];
@@ -178,12 +201,16 @@ function read_auth_by_grouptypes(JSONPath $masterdata_jsonpath, &$authdefinition
 
 
 /**
+ * This reads authentification by groups. Note that it walks along
+ * groupmemberstatus.
+ *
+ *
  * @param JSONPath $masterdata_jsonpath
  * @param $authdefinitions
  * @return array
  * @throws \Flow\JSONPath\JSONPathException
  */
-function read_auth_by_group(JSONPath $masterdata_jsonpath, &$authdefinitions)
+function read_auth_by_groups(JSONPath $masterdata_jsonpath, &$authdefinitions)
 {
     //$groups = find_in_JSONPath($masterdata_jsonpath,'$..groups.*');
 
@@ -192,31 +219,32 @@ function read_auth_by_group(JSONPath $masterdata_jsonpath, &$authdefinitions)
     $groupmissing = [];
     $groupauth = [];
 
-    foreach ($groupmemberauth as &$i) {
-        $hash = hash('md5', json_encode($i['auth']));
+    foreach ($groupmemberauth as &$authentry) {
+        $hash = hash('md5', json_encode($authentry['auth']));
 
-        $j = [
-            'group' => find_one_in_JSONPath($masterdata_jsonpath, "$..group.{$i['group_id']}.bezeichnung"),
+        $resolved_authentry = [
+            'group' => find_one_in_JSONPath($masterdata_jsonpath, "$..group.{$authentry['group_id']}.bezeichnung"),
             'role' => find_one_in_JSONPath($masterdata_jsonpath,
-                "$..grouptypeMemberstatus.[?(@.id=={$i['grouptype_memberstatus_id']})].bezeichnung"),
-            'group_id' => $i['group_id'],
-            'groupMemberstatus_id' => $i['id'],
+                "$..grouptypeMemberstatus.[?(@.id=={$authentry['grouptype_memberstatus_id']})].bezeichnung"),
+            'group_id' => $authentry['group_id'],
+            'groupMemberstatus_id' => $authentry['id'],
             'auth_hash' => $hash,
-            'auth' => resolve_auth_entry($i['auth'], $masterdata_jsonpath)
+            'auth' => $authentry['auth'],
+            'resolved_auth' => resolve_auth_entry($authentry['auth'], $masterdata_jsonpath)
         ];
 
 
-        if ($j['group'] == null) {
-            $groupmissing[$i['id']] = $i;
+        if ($resolved_authentry['group'] == null) {
+            $groupmissing[$authentry['id']] = $authentry;
+        } else {
+
+            $role = "GRRL {$resolved_authentry['group']} {$resolved_authentry['role']}";
+
+            $authentry = $resolved_authentry;
+            pushauthdef($hash, $role, $authentry['resolved_auth'], $authdefinitions);
+
+            $groupauth[$resolved_authentry['group']][$resolved_authentry['role']] = $resolved_authentry;
         }
-
-
-        $role = "Gruppe: {$j['group_id']} {$j['group']} ({$j['role']})}";
-
-        $i = $j;
-        pushauthdef($hash, $role, $i['auth'], $authdefinitions);
-
-        $groupauth[$j['group']][$j['role']] = $j;
     }
     return array($groupmissing, $groupauth);
 }
@@ -245,7 +273,21 @@ list($statusauth) = read_auth_by_status($masterdata_jsonpath, $authdefinitions);
 list($grouptypeauth) = read_auth_by_grouptypes($masterdata_jsonpath, $authdefinitions);
 
 // handle groups
-list($groupmissing, $groupauth) = read_auth_by_group($masterdata_jsonpath, $authdefinitions);
+list($groupmissing, $groupauth) = read_auth_by_groups($masterdata_jsonpath, $authdefinitions);
+
+// write file for simulation
+
+$rubyfile = fopen(__DIR__ . "/../responses/v1_churchauth--all.rb", "w");
+
+fwrite($rubyfile, "def mk_imported_auth(plan)\n");
+foreach ($authdefinitions as $authdefinition => $value) {
+    $defs = json_encode($value, JSON_UNESCAPED_UNICODE);
+    $record = "  plan.add('$authdefinition', [], $defs)\n";
+    fwrite($rubyfile, $record);
+}
+fwrite($rubyfile, "end\n");
+
+fclose($rubyfile);
 
 // report results
 
